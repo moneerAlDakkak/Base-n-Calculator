81ac4ceb35637414329cbcc796b131d5e39c4f42
Moneer Al-Dakkak <77343614+moneerAlDakkak@users.noreply.github.com>
2021-06-11 16:39:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let sign = document.getElementById("sign");
 
 sign.onclick = () => {
+  document.querySelector("select").classList.toggle("disabled");
+  if (document.querySelector("select").classList.contains("disabled")) {
+    document.querySelector("select").value = 2;
+    document.querySelector("select").disabled = true;
+  } else {
+    document.querySelector("select").disabled = false;
+  }
   sign.textContent === "+"
     ? (sign.textContent = "-")
     : (sign.textContent = "+");
@@ -70,5 +77,36 @@ function convertPositive() {
 }
 
 function convertNegative() {
-  console.log("good");
+  convertPositive();
+  let negative_1 = document.getElementById("result").textContent.split("");
+
+  console.log(negative_1); // 0 0
+
+  if (negative_1.length === 2) {
+    negative_1.unshift(0);
+  }
+
+  let negative = negative_1.map((digit) =>
+    digit == "0" ? (digit = 1) : (digit = 0)
+  );
+
+  console.log("before adding 1", negative); // 0 0 0
+
+  for (let i = negative.length - 1; i >= 0; i--) {
+    if (negative[i] == 1) {
+      negative[i] = 0;
+    } else {
+      negative[i] = 1;
+      break;
+    }
+  }
+
+  console.log("after adding 1", negative); // 1 0 0
+
+  negative.unshift(" ");
+  negative.unshift(1);
+  document.getElementById("result").textContent = "";
+  negative.forEach((digit) => {
+    document.getElementById("result").textContent += digit;
+  });
 }
